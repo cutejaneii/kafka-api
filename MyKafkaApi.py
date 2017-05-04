15f5febc48d44ad7a7e0ca04bf419aa384d3b10f
Jennifer <cutejaneii@hotmail.com>
2017-05-04 10:29:56 +0800

    Update MyKafkaApi.py

--- MyKafkaApi.py
+++ MyKafkaApi.py
@@ -10,27 +10,20 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 
 def index():
-	producer2 = KafkaProducer(bootstrap_servers=['127.0.0.1:9092'])
-	producer2.send('Test0220', b'1500')	
-	producer2.flush()
 	return "Index"
 
 @app.route('/add', methods=['POST'])
 
 def add():
-	print(type(request.json['data1']))
-	print(type(request.json['data1'].encode('utf-8')))
+	print(type(request.json['data']))
+	print(type(request.json['data'].encode('utf-8')))
 	print(request.json)
-	print(request.json['data1'])
+	print(request.json['data'])
 	producer = KafkaProducer(bootstrap_servers=['127.0.0.1:9092'])
-	producer.send('Test0220', request.json['data1'].encode('utf-8'))	
+	producer.send('topic_name', request.json['data'].encode('utf-8'))	
 	producer.flush()
 	return "add Data~"
         
 if __name__ ==  '__main__':
 	app.run(debug=True)      
         
-#import json
-#producer = KafkaProducer(value_serializer=lambda v: json.dumps(v).encode('UTF-8'))
-#producer.send('aa', {'key': 'value'})
-
